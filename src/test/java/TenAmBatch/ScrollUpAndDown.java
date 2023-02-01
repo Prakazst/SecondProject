d178098751a0aeeb8ea57e7631c60d705fa97e2c
Prakazst <124021013+Prakazst@users.noreply.github.com>
2023-02-01 10:14:56 +0530

    Update ScrollUpAndDown.java

--- src/test/java/TenAmBatch/ScrollUpAndDown.java
+++ src/test/java/TenAmBatch/ScrollUpAndDown.java
@@ -17,7 +17,9 @@ public class ScrollUpAndDown {
 		driver.manage().window().maximize();
 		
 		driver.get("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_5szpgfto9i_e&adgrpid=58075519359&hvpone=&hvptwo=&hvadid=486462756374&hvpos=&hvnetw=g&hvrand=5922133542991994462&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061896&hvtargid=kwd-64107830&hydadcr=14452_2154371&gclid=CjwKCAiAk--dBhABEiwAchIwkRhiRCHewBWfsNLDTvjsQhCeP9PkqJlEAc7T4p04eWJn5mwtTssh-xoCNNcQAvD_BwE");
-	
+		
+		Thread.sleep(2000);
+		
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		
 		WebElement downLandMark = driver.findElement(By.xpath("//div[contains(@class,'nav-logo-base')]"));
