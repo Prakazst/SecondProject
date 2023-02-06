225ef6935cbea1953298e2ab06674fd0b0d7ea0b
Prakazst <124021013+Prakazst@users.noreply.github.com>
2023-02-06 10:27:09 +0530

    Update ScrollUpAndDown.java

--- src/test/java/TenAmBatch/ScrollUpAndDown.java
+++ src/test/java/TenAmBatch/ScrollUpAndDown.java
@@ -13,22 +13,12 @@ public class ScrollUpAndDown {
 	public static void main(String[] args) throws InterruptedException {
 		
 		WebDriver driver = new ChromeDriver();
-		
 		driver.manage().window().maximize();
-		
 		driver.get("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_5szpgfto9i_e&adgrpid=58075519359&hvpone=&hvptwo=&hvadid=486462756374&hvpos=&hvnetw=g&hvrand=5922133542991994462&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061896&hvtargid=kwd-64107830&hydadcr=14452_2154371&gclid=CjwKCAiAk--dBhABEiwAchIwkRhiRCHewBWfsNLDTvjsQhCeP9PkqJlEAc7T4p04eWJn5mwtTssh-xoCNNcQAvD_BwE");
-		
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		
 		WebElement downLandMark = driver.findElement(By.xpath("//div[contains(@class,'nav-logo-base')]"));
-		Thread.sleep(3000);
-		
+		Thread.sleep(3000);	
 		jse.executeScript("arguments[0].scrollIntoView(true)", downLandMark);
-		
 		driver.quit();
-		
-		
-
 	}
-
 }
